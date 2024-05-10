3bf18db227b00d14a0691005b837c1cac1a00397
JamieH-alt <jamie.hanson@education.nsw.gov.au>
2024-05-10 11:58:44 +1000

    Fixed up the documentation

--- EV3/trig.py
+++ EV3/trig.py
@@ -1,20 +1,7 @@
-#!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port
-from pybricks.robotics import DriveBase
 import math
 
 robot_x = 0
 robot_y = 0
-direction = 0
-
-# Initialization
-ev3 = EV3Brick() 
-left_motor = Motor(Port.B)  # Change this to match.
-right_motor = Motor(Port.C)  # Change this to match.
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
-
 
 def location(x, y):
     global robot_x
@@ -26,12 +13,10 @@ def location(x, y):
     turn = math.atan2(atan_y, atan_x)
     degrees = math.degrees(turn)
     direction = degrees
-    robot.turn(degrees)
     print(degrees)  # Debugs how much it should turn by.
     print(direction)  # Debugs where it should be facing.
     distance = pow(pow(x - robot_x, 2) + pow(y - robot_y, 2), 0.5)
     print(distance)  # Debugs distance drove
-    robot.straight(distance)
     robot_x = x
     robot_y = y
 
@@ -39,7 +24,3 @@ location(100, 0)
 location(100, 100)
 location(0, 100)
 location(0, 0)
-#location(530, 460)
-#location(700 ,570)
-#location(700, 770)
-#location(760, 180)
